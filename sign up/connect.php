820fcec9d9f09b61d4815c55c22090e44eb7d335
warregielkens <91662820+warregielkens@users.noreply.github.com>
2022-12-07 09:26:07 +0100

    database ok

--- sign up/connect.php
+++ sign up/connect.php
@@ -1,11 +1,24 @@
 <?php 
-   
+    $email = $_POST['email'];
+    $psw = $_POST['psw'];
+    $psw_repeat = $_POST['psw_repeat'];
+    
     //database connection
-    $conn = new mysqli('193.121.129.31','warre','Warre6789!',);
+    $conn = new mysqli('193.121.129.31','warre','Warre6789!','signup');
     
     // Check connection
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
+  }
+  
+  $sql = "INSERT INTO signup (email, psw, psw_repeat)
+    VALUES ('$email', '$psw', '$psw_repeat')";
+
+    if ($conn->query($sql) === TRUE) {
+    echo "New sign up successfully";
+    } else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
     }
-    echo "Connected successfully";
+
+    $conn->close();
 ?>

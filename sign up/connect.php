9d425e0b788c66d56b7dd30484d5ece62edf3aff
warregielkens <91662820+warregielkens@users.noreply.github.com>
2023-01-10 17:39:34 +0100

    10-1-23
    no css
    go to page after submit signup

--- sign up/connect.php
+++ sign up/connect.php
@@ -1,21 +1,27 @@
 <?php 
+
+    $servername = "193.121.129.31";
+    $username = "warre";
+    $password = "Warre6789!";
+    $dbname = "signup";
+
     $email = $_POST['email'];
     $psw = $_POST['psw'];
     $psw_repeat = $_POST['psw_repeat'];
     
     //database connection
-    $conn = new mysqli('193.121.129.31','warre','Warre6789!','signup');
+    $conn = new mysqli($servername, $username, $password, $dbname);
     
     // Check connection
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
   }
   
-  $sql = "INSERT INTO signup (email, psw, psw_repeat)
-    VALUES ('$email', '$psw', '$psw_repeat')";
+  $sql = "INSERT INTO signup (email, psw, psw_repeat)  
+    VALUES ('$email', '$psw', '$psw_repeat')"; //insert values into db
 
     if ($conn->query($sql) === TRUE) {
-    echo "New sign up successfully";
+        header("Location:http://localhost:3000/index.html"); //go to page
     } else {
     echo "Error: " . $sql . "<br>" . $conn->error;
     }

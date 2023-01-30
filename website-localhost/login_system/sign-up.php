ce1b5c444c36b715bb07349875a80b0a7c094b6a
warregielkens <91662820+warregielkens@users.noreply.github.com>
2023-01-30 16:21:35 +0100

    footer + event page

--- website-localhost/login_system/sign-up.php
+++ website-localhost/login_system/sign-up.php
@@ -45,7 +45,7 @@ $conn = mysqli_connect($servername, $username, $password, $dbname);
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <title>Sign Up</title>
-      <link rel="stylesheet" href="css/sign-up.css">
+      <link rel="stylesheet" href="css-login-system/sign-up.css">
       
   </head>
   <body>
--- website-one.com/website/login_system/sign-up.php
+++ website-one.com/website/login_system/sign-up.php
@@ -47,7 +47,7 @@
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <title>Sign Up</title>
-      <link rel="stylesheet" href="css/sign-up.css">
+      <link rel="stylesheet" href="css-login-system/sign-up.css">
       
   </head>
   <body>

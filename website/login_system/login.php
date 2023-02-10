7f177eb3d106658f8b9accf9cb8c11a7c2e23888
warregielkens <91662820+warregielkens@users.noreply.github.com>
2023-02-10 23:03:44 +0100

    buh

--- website/login_system/login.php
+++ website/login_system/login.php
@@ -40,6 +40,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+    <link rel="icon" href="images/logo_tab.png">
     <link rel="stylesheet" href="css-login-system/login.css">
 </head>
 <body>
--- website/login_system/sign-up.php
+++ website/login_system/sign-up.php
@@ -47,6 +47,7 @@
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <title>Sign Up</title>
+      <link rel="icon" href="images/logo_tab.png">
       <link rel="stylesheet" href="css-login-system/sign-up.css">
       
   </head>

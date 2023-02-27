5c7593cbfe646fb41995b14b6fb3b59c63e38871
SupremeGlitching <108416245+SupremeGlitching@users.noreply.github.com>
2023-02-27 14:03:40 +0100

    Licka my asshole

--- app/app/src/main/java/com/example/myloginapp/Activity3.java
+++ app/app/src/main/java/com/example/myloginapp/Activity3.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Patterns;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -18,7 +19,39 @@ import org.w3c.dom.Text;
 
 public class Activity3 extends AppCompatActivity {
 
+    EditText usernamesign;
+    EditText passwordsign;
+    EditText confirmPass;
     EditText email;
+    Button confirmsignup;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_3);
+        usernamesign = findViewById(R.id.usernamesign);
+        passwordsign = findViewById(R.id.passwordsign);
+        confirmPass = findViewById(R.id.confirmPass);
+        email = findViewById(R.id.email);
+        confirmsignup = findViewById(R.id.confirmsignup);
+
+        confirmsignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = usernamesign.getText().toString();
+                String password = passwordsign.getText().toString();
+                String confirmPassword = confirmPass.getText().toString();
+                String Email = email.getText().toString();
+                String url = "http://193.121.129.31/GIP-2022/insertData.php";
+                String type = "register";
+                BackgroundWorker backgroundWorker = new BackgroundWorker(Activity3.this);
+                backgroundWorker.execute(url, type, name, password, Email);
+            }
+        });
+    }
+}
+
+    /*EditText email;
     EditText usernamesign;
     boolean EmailValid = false;
     boolean UsernameValid = false;
@@ -92,4 +125,4 @@ public class Activity3 extends AppCompatActivity {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
-}
+}*/
--- app/app/src/main/java/com/example/myloginapp/MainActivity.java
+++ app/app/src/main/java/com/example/myloginapp/MainActivity.java
@@ -52,6 +52,7 @@ public class MainActivity extends AppCompatActivity {
     public void openActivity2() {
         Intent intent = new Intent(this, Activity2.class);
         startActivity(intent);
+
     }
 
     public void openActivity3() {

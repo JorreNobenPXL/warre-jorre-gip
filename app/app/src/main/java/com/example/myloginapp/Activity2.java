3ef37ccafe02dd606f27e8e5f26d70abca0f71db
SupremeGlitching <108416245+SupremeGlitching@users.noreply.github.com>
2023-05-11 15:14:08 +0200

    HONKY PLANKY

--- app/app/src/main/java/com/example/myloginapp/Activity2.java
+++ app/app/src/main/java/com/example/myloginapp/Activity2.java
@@ -3,20 +3,27 @@ package com.example.myloginapp;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 public class Activity2 extends AppCompatActivity {
 
-    //TextView reciever_msg;
+    TextView reciever_msg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        /*setContentView(R.layout.activity_2);
+        setContentView(R.layout.activity_2);
+
+        setContentView(R.layout.activity_2);
 
         reciever_msg = findViewById(R.id.received_value_id);
         Intent intent = getIntent();
-        String str = intent.getStringExtra("Login");
-        reciever_msg.setText(str);*/
+        String str = intent.getStringExtra("NAME");
+        reciever_msg.setText(str);
+        Log.d("AIDS VLEK", str);
+
+
+
     }
 }
